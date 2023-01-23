c1fc072740e0f85b48bc1337be59e58d9b0cfa1f
papaikumarpaul <120701133+papaikumarpaul@users.noreply.github.com>
2023-01-24 02:47:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,29 +33,3 @@ close_btn.addEventListener('click', () => {
 	social_panel_container.classList.remove('visible')
 });
 
-
-// function hideAllContents() {
-// 	contents.forEach(content => {
-// 		content.classList.remove('show');
-// 	});
-// }
-
-// function hideAllItems() {
-// 	listItems.forEach(item => {
-// 		item.classList.remove('active');
-// 	});
-// }
-
-
-// // SOCIAL PANEL JS
-// const floating_btn = document.querySelector('.floating-btn');
-// const close_btn = document.querySelector('.close-btn');
-// const social_panel_container = document.querySelector('.social-panel-container');
-
-// floating_btn.addEventListener('click', () => {
-// 	social_panel_container.classList.toggle('visible')
-// });
-
-// close_btn.addEventListener('click', () => {
-// 	social_panel_container.classList.remove('visible')
-// });
